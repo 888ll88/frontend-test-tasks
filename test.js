e1f0824f435ae4abeccdc9d7a5348e94343fd791
888ll88 <79980985+888ll88@users.noreply.github.com>
2024-07-18 15:51:03 +0300

    Update test.js
    Using numbers twice in a couple except first & last

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 function transformString(str) {
-    str = str.replace(/\s+/g, ''); // Видалення всіх пробілів з вхідного рядка
+    // Видалення всіх пробілів з вхідного рядка
+    str = str.replace(/\s+/g, ''); // Замінює всі пробіли в рядку на порожній рядок
 
     // Функція для отримання третьої букви, яка не є рівною a або b
     const getThirdLetter = (a, b) => {
@@ -9,18 +10,20 @@ function transformString(str) {
 
     while (str.length > 1) { // Поки довжина рядка більше 1
         let newStr = ''; // Новий рядок для зберігання результату
-        for (let i = 0; i < str.length; i += 2) { // Проходимо по рядку з кроком 2
+        for (let i = 0; i < str.length - 1; i += 1) { // Проходимо по рядку з кроком 2
             let a = str[i]; // Поточна буква a
-            let b = str[i + 1] !== undefined ? str[i + 1] : str[i - 1]; // Наступна буква b або передостання буква, якщо b undefined. Якщо в рядку не парна кільість букв
+            let b = str[i + 1]; // Наступна буква b
             newStr += (a === b) ? a : getThirdLetter(a, b); // Якщо букви однакові, додаємо a, інакше додаємо третю букву
         }
         str = newStr; // Оновлюємо str новим рядком
-        console.log(newStr) // Для наглядного розбору
+        console.log(newStr)
     }
 
     return str; // Повертаємо останній символ
 }
 
-let inputString = "RGB"; // Рядок букв
+// Приклад використання
+let inputString = "GRRBRGBB"; // Вхідний рядок з пробілами
 let result = transformString(inputString); // Виклик функції та збереження результату
+
 console.log(result); // Виведення результату в консоль
